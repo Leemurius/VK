97729e11df038a7f316636103bed495b0b2079de
leemurus <sidorevich.anton@gmail.com>
2019-11-20 21:05:31 +0300

    Added beautiful animation for 'profile-box'
    - Added the ability to enter a profile through any picture

--- app/static/js/main/profile.js
+++ app/static/js/main/profile.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
    addSelfInformationInProfileBox(); // Default for profile box
    $('.box-for-all').removeClass('col-xl-9').addClass('col-xl-6');
+   $('.profile-box').removeClass('col-xl-1').addClass('col-xl-3');
    $('.profile-box').show();
    $('.card-header .big-window').remove();
+   LastLiClick = true;
 });
-
-LastTrClick = true;
--- app/static/js/main/search.js
+++ app/static/js/main/search.js
@@ -1,25 +1,3 @@
-var LastTrClick = undefined;
-$('tr[data-href]').on("click", function() {
-    const username = $(this).attr("data-href");
-
-    if (LastTrClick == username) {
-        $('.box-for-all').removeClass('col-xl-6').addClass('col-xl-9');
-        $('.profile-box').hide();
-        LastTrClick = undefined;
-    } else {
-        addInformationInProfileBox(username);
-        $('.box-for-all').removeClass('col-xl-9').addClass('col-xl-6');
-        $('.profile-box').show();
-        LastTrClick = username;
-    }
-});
-
-$(".write_message a").click(function() {
-    var username = $(this).attr("href");
-    var room_id = getAjaxInformation('http://' + getServerName() + '/api/rooms/' + getProfileId(username));
-   $(".write_message a").attr('href', 'chat/' + room_id);
-});
-
 
 $(".search-person").click(function (e) {
     // TODO: realize this:(
